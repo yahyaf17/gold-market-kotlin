fa8bd30254a90644dd6b126daf6d5322e37f505c
Yahya Faikar Hanif <yahyafaikar17@gmail.com>
2021-07-17 16:31:56 +0700

    layout splash screen and welcome screen

---
+++ app/src/main/java/com/mandiri/goldmarket/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.mandiri.goldmarket.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mandiri.goldmarket.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/mandiri/goldmarket/activity/WelcomeActivity.kt
+++ app/src/main/java/com/mandiri/goldmarket/activity/WelcomeActivity.kt
@@ -1,10 +1,12 @@
 package com.mandiri.goldmarket.activity
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import com.mandiri.goldmarket.R
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,9 @@ class WelcomeActivity : AppCompatActivity() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContentView(R.layout.activity_welcome)
 
+        btnGetStarted.setOnClickListener {
+            val toLoginActivity = Intent(this@WelcomeActivity, LoginActivity::class.java)
+            startActivity(toLoginActivity)
+        }
     }
 }
